7b5e304ce33acaf57f23203161825e542390d015
Nandha <nandha.kumar790@gmail.com>
2024-05-09 01:01:03 +0530

    project scope modified

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
-import testRepeat from '../lib/index.js';
+import main from '../lib/index.js';
 
 // Delete the 0 and 1 argument (node and script.js)
 var args = process.argv.splice(process.execArgv.length + 2);
 
-// Retrieve the first argument
-var input = args[0];
-
-testRepeat(input);
+// pass the arguments to function
+main(args);
